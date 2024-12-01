297a3714cd504459a165ea86ccb3bd1eeb560739
shishirRsiam <shishir.siam01@gmail.com>
2024-12-02 01:05:18 +0600

    Authenticator_App Fix Done

---
+++ Phitron/Module Project/_4_CurHub_Project/Authenticator_App/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from Authenticator_App import views as Authenticator_App_Views
+
+
+urlpatterns = [
+    path('profile/', Authenticator_App_Views.profile, name='profile'),
+    path('edit/profile/', Authenticator_App_Views.edit_profile, name='edit_profile'),
+    path('edit/password/', Authenticator_App_Views.change_password, name='change_password'),
+    path('login/', Authenticator_App_Views.login_page, name='login'),
+    path('logout/', Authenticator_App_Views.logout_page, name='logout'),
+    path('signup/', Authenticator_App_Views.signup_page, name='signup'),
+]
