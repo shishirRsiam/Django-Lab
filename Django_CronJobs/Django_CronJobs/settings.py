1b89ea8cc85d6072d8119a61e6403acd60f3ae67
shishirRsiam <shishir.siam01@gmail.com>
2025-06-02 00:14:23 +0600

    use function based view for better. class based view some problem git push

--- Django_CronJobs/Django_CronJobs/settings.py
+++ Django_CronJobs/Django_CronJobs/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Cron', 'django_crontab', 
+    'Cron', 'django_crontab',
 ]
 
 MIDDLEWARE = [
